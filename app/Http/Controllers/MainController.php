16cb01bf24f16fa5349147168f0b58bc2a9bbc56
Dantedod <danted2300@gmail.com>
2025-03-20 16:04:58 -0300

    feat:make finalResult layout and method for show results

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -174,7 +174,14 @@ class MainController extends Controller
 
   public function showResults()
   {
-    echo "mostrar resultados finais";
-    dd(session()->all());
+    $total_questions = session('total_questions');
+    $correct_answers = session('correct_answers');
+
+    return view('final_result')->with([
+      'correct_answers' =>  session('correct_answers'),
+      'wrong_answers' =>  session('wrong_answers'),
+      'total_questions' =>  session('total_questions'),
+      'percentage' => round($correct_answers / $total_questions * 100, 2)
+    ]);
   }
 }
